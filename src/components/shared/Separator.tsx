ca148e47d65f33092e640d908d96ba01317eb573
Will Andree <williamandree@gmail.com>
2026-08-14 10:58:24 -0500

    Added separator to Memberships page.

--- src/components/shared/Separator.tsx
+++ src/components/shared/Separator.tsx
@@ -14,14 +14,14 @@ const Separator = () => {
           <RevealAnimation delay={0.2} offset={10}>
             <a
               href="#membership-form"
-              className="btn btn-md btn-accent border-[#f2f2f2] before:hidden basis-[200px] inline-flex items-center gap-2 duration-300 hover:shadow-md shadow-black/5 hover:bg-[#f8f8f8]">
+              className="btn btn-md btn-accent before:hidden  inline-flex items-center gap-2 duration-300 hover:shadow-md shadow-black/5 bg-fr-primary-700  text-white hover:bg-fr-primary-800">
               Inquire
               <svg
                 className="size-[15px] -mb-2 -mr-2"
                 xmlns="http://www.w3.org/2000/svg"
                 width="32"
                 height="32"
-                fill="#000000"
+                fill="#fff"
                 viewBox="0 0 256 256">
                 <path d="M229.66,165.66l-48,48a8,8,0,0,1-11.32,0l-48-48a8,8,0,0,1,11.32-11.32L168,188.69V72H32a8,8,0,0,1,0-16H176a8,8,0,0,1,8,8V188.69l34.34-34.35a8,8,0,0,1,11.32,11.32Z"></path>
               </svg>
